05913f4c1316a6cb281977963bdaa40f6dce8e77
Waseem Kurne <kurne.waseem@gmail.com>
2023-10-23 12:02:25 +0530

    Feed Post Code Split

--- src/components/FeedPosts.js
+++ src/components/FeedPosts.js
@@ -1,13 +1,6 @@
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  FlatList,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native";
-import { Feather, AntDesign } from "@expo/vector-icons";
+import { View, FlatList, StyleSheet } from "react-native";
+import Post from "./Post";
 
 const FeedPosts = ({ feedPosts }) => {
   const [likedPosts, setLikedPosts] = useState([]);
@@ -18,36 +11,6 @@ const FeedPosts = ({ feedPosts }) => {
     } else {
       setLikedPosts([...likedPosts, postId]);
     }
-  };
-
-  const Post = ({ post, isLiked, onLikeToggle }) => {
-    const { id, userImage, imageUrl, username, likes, caption, timestamp } = post;
-
-    return (
-      <View style={styles.postContainer}>
-        <View style={styles.postHeader}>
-          <Image source={{ uri: userImage }} style={styles.userImage} />
-          <Text style={styles.username}>{username}</Text>
-        </View>
-        <Image source={{ uri: imageUrl }} style={styles.postImage} />
-        <View style={styles.postDetails}>
-          <View style={styles.iconsContainer}>
-            <TouchableOpacity onPress={() => onLikeToggle(id)} style={styles.icons}>
-              <AntDesign name={isLiked ? "heart" : "hearto"} size={28} color={isLiked ? "red" : "black"} />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.icons}>
-              <Feather name="message-circle" size={28} color="black" />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.icons}>
-              <Feather name="send" size={28} color="black" />
-            </TouchableOpacity>
-          </View>
-          <Text style={styles.likes}>{likes} likes</Text>
-          <Text style={styles.caption}>{caption}</Text>
-          <Text style={styles.timestamp}>{timestamp}</Text>
-        </View>
-      </View>
-    );
   };
 
   return (
@@ -70,48 +33,6 @@ const FeedPosts = ({ feedPosts }) => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
-  },
-  postContainer: {
-    marginBottom: 20,
-  },
-  postHeader: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 10,
-  },
-  userImage: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    marginRight: 10,
-  },
-  username: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  postImage: {
-    width: "100%",
-    height: 300,
-  },
-  postDetails: {
-    paddingHorizontal: 10,
-  },
-  iconsContainer: {
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    marginVertical: 5,
-  },
-  icons: {
-    paddingRight: 15,
-  },
-  likes: {
-    fontWeight: "bold",
-  },
-  caption: {
-    marginBottom: 5,
-  },
-  timestamp: {
-    color: "gray",
   },
 });
 
